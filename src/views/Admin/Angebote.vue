05cfdda3ef9a219c7e49488ecf9ac4abd0997c3a
maurb <maurice@buesching.eu>
2020-12-15 12:32:12 +0100

    navbar fix

--- src/views/Admin/Angebote.vue
+++ src/views/Admin/Angebote.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Angebotsübersicht</h1>
-  <nav class="form-group row">
-    <div class="col-sm-4">
+  <nav class="form-group d-flex justify-content-between">
+    <div class="col-sm-3">
       <select class="form-control" >
         <option value="1" selected>Alle</option>
         <option value="2">Neu</option>
@@ -9,8 +9,7 @@
         <option value="3">Abgeschlossen</option>
       </select>
     </div>
-
-    <div class="input-group ml-auto">
+    <div class="input-group col-sm-2">
       <input class="form-control my-0 py-1 lime-border" type="text" placeholder="Search" aria-label="Search">
       <div class="input-group-append">
           <span class="input-group-text lime lighten-2" id="basic-text1"><i class="fas fa-search text-grey"
