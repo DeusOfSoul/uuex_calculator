25b9c90a602c1a7f00ca296bd9960456ccabec35
Siktik <74296285+Siktik@users.noreply.github.com>
2020-12-16 12:23:03 +0100

    Update objectList.js
    multiplikatoren aufgenommen und Fläche hinzugefügt, die ist ja nur default wert

--- src/assets/data/admin/objectList.js
+++ src/assets/data/admin/objectList.js
@@ -2,17 +2,64 @@ export const objectListe = [
     {
         id: 1,
         name: "Haus",
-        preisNormal: 10,
-        preisExtrem: 20,
-        preisMessi: 30,
+        flaeche: 200,
+        multNormal: 16, 
+        multExtrem: 26,
+        multMessi: 42,
         levelNeeded: false
     },
-    {
+     {
         id: 2,
-        name: "Wohnzimmer",
-        preisNormal: 10,
-        preisExtrem: 20,
-        preisMessi: 30,
-        levelNeeded: true
-    }
+        name: "Zimmer",
+        flaeche: 50,
+        multNormal: 16, 
+        multExtrem: 26,
+        multMessi: 42,
+        levelNeeded: false
+    },
+     {
+        id: 3,
+        name: "Keller",
+        flaeche: 20,
+        multNormal: 18, 
+        multExtrem: 28,
+        multMessi: 46,
+        levelNeeded: false
+    },
+     {
+        id: 4,
+        name: "Garage",
+        flaeche: 20,
+        multNormal: 18, 
+        multExtrem: 28,
+        multMessi: 48,
+        levelNeeded: false
+    },
+     {
+        id: 5,
+        name: "Dachboden",
+        flaeche: 30,
+        multNormal: 21, 
+        multExtrem: 31,
+        multMessi: 52,
+        levelNeeded: false
+    },
+     {
+        id: 6,
+        name: "Küche",
+        flaeche: 20,
+        multNormal: 26, 
+        multExtrem: 36,
+        multMessi: 62,
+        levelNeeded: false
+    },
+     {
+        id: 7,
+        name: "Wohnung",
+        flaeche: 100,
+        multNormal: 16, 
+        multExtrem: 26,
+        multMessi: 42,
+        levelNeeded: false
+    } 
 ]
