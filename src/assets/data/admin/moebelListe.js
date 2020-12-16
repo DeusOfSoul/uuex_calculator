7581b640b8ef7fcb3076c9aeaa2dce6373917e58
Siktik <74296285+Siktik@users.noreply.github.com>
2020-12-16 12:11:57 +0100

    Update moebelListe.js
    Neue Einträge in die Liste , Größe jetzt 21

--- src/assets/data/admin/moebelListe.js
+++ src/assets/data/admin/moebelListe.js
@@ -2,8 +2,8 @@
 export const moebelListe = [
     {
         id: 1,
-        name: "Tisch",
-        volume: 1.5,
+        name: "Tisch-normal",
+        volume: 2.5,
         abbaubar: false
     },
     {
@@ -14,8 +14,104 @@ export const moebelListe = [
     },
     {
         id: 4,
-        name: "Tisch",
+        name: "Tisch-klein",
         volume: 1.5,
+        abbaubar: false
+    },
+    {
+        id: 5,
+        name: "Stuhl",
+        volume: 1.5,
+        abbaubar: false
+    },
+    {
+        id: 6,
+        name: "Sessel",
+        volume: 2.5,
+        abbaubar: false
+    },
+    {
+        id: 7,
+        name: "Zweisitzer-Couch",
+        volume: 4.0,
+        abbaubar: false
+    },
+    {
+        id: 8,
+        name: "Dreisitzer-Couch",
+        volume: 6.0,
+        abbaubar: false
+    },
+    {
+        id: 9,
+        name: "Eck-Couch",
+        volume: 9.5,
+        abbaubar: True
+    },
+    {
+        id: 10,
+        name: "Eck-Bank",
+        volume: 6,
+        abbaubar: True
+    },
+    {
+        id: 11,
+        name: "Schrank-groß",
+        volume: 3.5,
+        abbaubar: True
+    },
+    {
+        id: 13,
+        name: "Waschmaschine",
+        volume: 2.5,
+        abbaubar: false
+    },
+    {
+        id: 14,
+        name: "Spülmaschine",
+        volume: 2.5,
+        abbaubar: false
+    },
+    {
+        id: 15,
+        name: "Bank-Esszimmer",
+        volume: 3.5,
+        abbaubar: false
+    },
+    {
+        id: 16,
+        name: "Gartenbank",
+        volume: 2.5,
+        abbaubar: false
+    },
+    {
+        id: 17,
+        name: "Spiegel",
+        volume: 1.0,
+        abbaubar: false
+    },
+    {
+        id: 18,
+        name: "Lautsprecher",
+        volume: 1.0,
+        abbaubar: false
+    },
+    {
+        id: 19,
+        name: "Grill",
+        volume: 2.0,
+        abbaubar: false
+    },
+    {
+        id: 20,
+        name: "Stehlampe",
+        volume: 1.0,
+        abbaubar: false
+    },
+    {
+        id: 21,
+        name: "Nachttisch",
+        volume: 0.5,
         abbaubar: false
     }
 ]
